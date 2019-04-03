094df282054d6047a5d82a19d96f34bce9f0d969
201851021-iiitv <49015110+201851021-iiitv@users.noreply.github.com>
2019-04-03 15:29:41 -0400

    Add files via upload

--- RedBlackTree/src/NodeAvl.java
+++ RedBlackTree/src/NodeAvl.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+//color>black=0;
+//color >red=1;
+public class NodeAvl<D> {
+    D value;
+    NodeAvl<D> left,right,parent;
+    int color;
+    NodeAvl(D value){
+        this.value=value;
+        left=right=null;
+        color=0;
 
-public class NodeAvl {
+    }
 }
